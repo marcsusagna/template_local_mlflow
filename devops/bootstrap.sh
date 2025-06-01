04f4c3a13ae42d3a310792efe5beba45b1c1d74e
marcsusagna <marcsusagna@gmail.com>
2025-06-01 19:50:11 +0200

    add precommit

--- devops/bootstrap.sh
+++ devops/bootstrap.sh
@@ -9,6 +9,8 @@ python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements_installer.txt
 uv sync
 
+pre-commit install
+
 nohup bash ./tasks/mlflow_server.sh &> logs/server_logs.log &
 
 python3 -m tasks.experiment_creation --exp_name "$init_exp_name"
--- devops/tasks/experiment_creation.py
+++ devops/tasks/experiment_creation.py
@@ -1,5 +1,4 @@
 import argparse
-from pathlib import Path
 import logging
 
 import mlflow
@@ -11,19 +10,24 @@ VERSION = "v1"
 
 # Logger
 logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s"
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
 )
 logger = logging.getLogger(__name__)
 
+
 def main():
     parser = argparse.ArgumentParser(description="Create an MLflow experiment")
-    parser.add_argument("--exp_name", type=str, required=True, help="Name of the MLflow experiment")
+    parser.add_argument(
+        "--exp_name",
+        type=str,
+        required=True,
+        help="Name of the MLflow experiment",
+    )
     args = parser.parse_args()
 
     # Var resolution
-    exp_tags={"version": VERSION}
-    exp_name=args.exp_name
+    exp_tags = {"version": VERSION}
+    exp_name = args.exp_name
 
     # Set MLFlow
     mlflow.set_tracking_uri(uri=TRACKING_URI)
@@ -34,6 +38,7 @@ def main():
         tags=exp_tags,
     )
     logger.info(f"Created experiment '{exp_name}' with ID: {experiment_id}")
+
 
 if __name__ == "__main__":
     main()
