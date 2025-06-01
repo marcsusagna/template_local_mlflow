0b9dce44e0da4fb1433564eb96a21568d51cd179
marcsusagna <marcsusagna@gmail.com>
2025-06-01 19:35:37 +0200

    initial commit

---
+++ devops/bootstrap.sh
@@ -0,0 +1,14 @@
+venv_name=.venv
+init_exp_name=template_exp
+
+python3 -m venv "$venv_name"
+source "$venv_name/bin/activate"
+
+cd devops 
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements_installer.txt
+uv sync
+
+nohup bash ./tasks/mlflow_server.sh &> logs/server_logs.log &
+
+python3 -m tasks.experiment_creation --exp_name "$init_exp_name"
